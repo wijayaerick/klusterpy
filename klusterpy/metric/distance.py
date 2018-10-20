ee290fa65862adcefb3debdee6ca39674a38ca16
Erick Wijaya <wijaya.erick52@gmail.com>
2018-10-20 20:31:12 +0700

    Update distance.py

--- klusterpy/metric/distance.py
+++ klusterpy/metric/distance.py
@@ -4,12 +4,10 @@ def euclidean_dist(a, b):
 	dist = 0
 	for i in range(len(a)):
 		dist += (a[i]-b[i])*(a[i]-b[i])
-	
 	return sqrt(dist)
 
-def manhattan_dist(x, y):
+def manhattan_dist(a, b):
 	dist = 0
 	for i in range(len(a)):
 		dist += abs(a[i]-b[i])
-	
 	return dist
